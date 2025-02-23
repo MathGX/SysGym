fe8feced4bc7cbf4b6033a50076e6d702fc5e876
MathGX <arielcapo016@gmail.com>
2025-02-23 16:21:59 -0300

    Credenciales de BD

--- others/conexion/conexion.php
+++ others/conexion/conexion.php
@@ -1,11 +1,11 @@
 <?php
 class Conexion
 {
-   private $host = "localhost";
-   private $port = "5433";
+   private $host = "10.100.99.1";
+   private $port = "5432";
    private $dbname = "SysGym";
    private $user = "postgres";
-   private $password = "123";
+   private $password = "Pokemon29";
    private $conexion;
 
    function getConexion()
